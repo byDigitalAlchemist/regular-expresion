e030a731dc96e97c44c8200ffeb8bbd15609d1c6
Digital Alchemist <developer.anwar@gmail.com>
2017-03-04 19:34:40 -0500

    Regular Expression Problem for CareerCup Added.

---
+++ RegularExpression/src/main/java/com/dc/engineering/re/CareerCupResolver.java
@@ -0,0 +1,22 @@
+package com.dc.engineering.re;
+
+public class CareerCupResolver {
+
+	String regularExpression;
+	String userInput;
+	
+	public CareerCupResolver(){
+		regularExpression = "";
+		userInput = "";
+	}
+	
+	public CareerCupResolver(String regularExpression, String userInput){
+		this.regularExpression = regularExpression;
+		this.userInput = userInput;
+	}
+	
+	public void userInput(String regularExpression, String validateUserInput){
+		
+	}
+	
+}
