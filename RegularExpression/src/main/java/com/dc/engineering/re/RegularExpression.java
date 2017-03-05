ba5d3af4c8061ecb9f819a1248c40fd0179d87f2
Digital Alchemist <developer.anwar@gmail.com>
2017-03-04 19:18:04 -0500

    Regular Expression 1.0
    Basic
    Advanced

---
+++ RegularExpression/src/main/java/com/dc/engineering/re/RegularExpression.java
@@ -0,0 +1,21 @@
+package com.dc.engineering.re;
+
+public class RegularExpression {
+
+	public static final String EXAMPLE_TEST = "This is my small example " + "string which I'm going to "
+			+ "use for pattern matching.";
+
+	public static void main(String[] args) {
+		System.out.println(EXAMPLE_TEST.matches("\\w.*"));
+		
+		String[] splitString = (EXAMPLE_TEST.split("\\s+"));
+		
+		System.out.println(splitString.length);// should be 14
+		
+		for (String string : splitString) {
+			System.out.println(string);
+		}
+		// replace all whitespace with tabs
+		System.out.println(EXAMPLE_TEST.replaceAll("\\s+", "\t"));
+	}
+}
