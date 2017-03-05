2076213a0364882acbd34ca88ee784c9c7eedf5f
Digital Alchemist <developer.anwar@gmail.com>
2017-03-05 18:17:16 -0500

    Unit Test Version Pom 4
    Test Cases Added

---
+++ RegularExpression/src/main/java/com/dc/engineering/re/problems/HTMLTags.java
@@ -0,0 +1,9 @@
+package com.dc.engineering.re.problems;
+
+public class HTMLTags {
+	
+	public void detect(String userText){
+		
+		String pattern = "<//s";
+	}
+}
---
+++ RegularExpression/src/test/java/com/dc/engineering/re/HTMLTagsTest.java
@@ -0,0 +1,13 @@
+package com.dc.engineering.re;
+
+import org.junit.Test;
+
+public class HTMLTagsTest {
+
+	
+	@Test
+	public void testAnchor(){
+		
+	}
+	
+}
