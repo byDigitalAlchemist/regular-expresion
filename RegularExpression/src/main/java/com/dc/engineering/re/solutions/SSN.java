197371523324d05104af218b1fe70f34647f038b
Digital Alchemist <developer.anwar@gmail.com>
2017-03-06 21:28:14 -0500

    Social security number details

--- RegularExpression/src/main/java/com/dc/engineering/re/solutions/SSN.java
+++ RegularExpression/src/main/java/com/dc/engineering/re/solutions/SSN.java
@@ -1,8 +1,20 @@
 package com.dc.engineering.re.solutions;
 
+
+/**
+ * A Social Security number CANNOT : 
+ * 
+ * Contain all zeroes in any specific group (ie 000-##-####, ###-00-####, or ###-##-0000)
+ * Begin with ’666′.
+ * Begin with any value from ’900-999′
+ * Be ’078-05-1120′  (due to the Woolworth’s Wallet Fiasco)
+ * Be ’219-09-9999′ (appeared in an advertisement for the Social Security Administration)
+ * 
+ * @author Digital Alchemist
+ */
 public class SSN {
 	
-	public boolean isValidSSN(String userText){
+	public static boolean isValidSSN(String userText){
 		
 		return false;
 	}
