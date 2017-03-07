8e8e8db12bb9ecc24f821cfa572482d00341c78a
Digital Alchemist <developer.anwar@gmail.com>
2017-03-06 21:57:44 -0500

    SSN updates with commentary

--- RegularExpression/src/main/java/com/dc/engineering/re/App.java
+++ RegularExpression/src/main/java/com/dc/engineering/re/App.java
@@ -12,7 +12,7 @@ public class App {
 	
     public static void main( String[] args ){
 
-    	System.out.println(" SSN 1: " + SSN.isValidSSN("123-453-6789") );
+    	System.out.println(" SSN 1: " + SSN.isValidSSN("1234567890") );
     	
     	
     }
--- RegularExpression/src/main/java/com/dc/engineering/re/solutions/SSN.java
+++ RegularExpression/src/main/java/com/dc/engineering/re/solutions/SSN.java
@@ -18,13 +18,25 @@ public class SSN {
 	
 	public static final String first_pattern = "\\d{3}-\\d{3}-\\d{4}";
 	
+	/**
+	 * avoid garbage start and ending characters like aaa333-338-2983449dss
+	 */
 	public static final String second_pattern = "^\\d{3}-\\d{3}-\\d{4}$";
 	
-	public static final String third_pattern = "^\\d{3}-\\d{3}-\\d{4}$";
+	/**
+	 * 333-333-3333 or 3333333333 both Valid
+	 * 333-3333333 invalid - but its allowed here
+	 */
+	public static final String third_pattern = "^\\d{3}-?\\d{3}-?\\d{4}$";
+	
+	/**
+	 * only and only 333-333-3333 or 3333333333 valid
+	 */
+	public static final String fourth_pattern = "^\\d{3}(-\\d{3}-|\\d{3})\\d{4}$";
 	
 	
 	public static boolean isValidSSN(String userText){
-		Pattern p = Pattern.compile(second_pattern);
+		Pattern p = Pattern.compile(fourth_pattern);
 		Matcher matcher = p.matcher(userText);
 		
 		if(matcher.find())
