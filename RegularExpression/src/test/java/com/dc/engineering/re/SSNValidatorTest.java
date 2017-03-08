f6fe1576a11e748888d37fd23310602bd6b2902d
Digital Alchemist <developer.anwar@gmail.com>
2017-03-08 00:10:46 -0500

    SSN validation test added

--- RegularExpression/src/test/java/com/dc/engineering/re/SSNValidatorTest.java
+++ RegularExpression/src/test/java/com/dc/engineering/re/SSNValidatorTest.java
@@ -12,8 +12,16 @@ public class SSNValidatorTest {
 	public void validate(){
 		
 		assertFalse(SSN.isValidSSN("123456789"));
+		assertFalse(SSN.isValidSSN("123-456-789"));
+		
 		assertTrue(SSN.isValidSSN("1234567890"));
-		assertTrue(SSN.isValidSSN("6664567890"));
+		assertTrue(SSN.isValidSSN("123-456-7890"));
+		
+		assertFalse(SSN.isValidSSN("6664567890"));
+		assertFalse(SSN.isValidSSN("666-456-7890"));
+		
+		assertTrue(SSN.isValidSSN("166-456-7890"));
+	
 	}
 
 }
