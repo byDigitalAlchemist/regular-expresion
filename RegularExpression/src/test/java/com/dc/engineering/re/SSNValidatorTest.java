461d48d2deef657353c60294857802e09bd0a9e0
Digital Alchemist <developer.anwar@gmail.com>
2017-03-10 19:55:57 -0500

    SSN Validation completed
    Signed-off-by: Digital Alchemist <developer.anwar@gmail.com>

--- RegularExpression/src/test/java/com/dc/engineering/re/SSNValidatorTest.java
+++ RegularExpression/src/test/java/com/dc/engineering/re/SSNValidatorTest.java
@@ -11,7 +11,7 @@ import com.dc.engineering.re.solutions.SSN;
 public class SSNValidatorTest {
 
 	@Test
-	public void validateDashSSN(){
+	public void validateDashSSN() {
 		assertFalse(SSN.isValidSSN("123-456-789"));
 		assertTrue(SSN.isValidSSN("123456789"));
 		assertTrue(SSN.isValidSSN("123-45-6789"));
@@ -21,29 +21,28 @@ public class SSNValidatorTest {
 		assertTrue(SSN.isValidSSN("166-45-7890"));
 		assertTrue(SSN.isValidSSN("616-45-7890"));
 		assertTrue(SSN.isValidSSN("166-46-7890"));
-	
-	assertFalse(SSN.isValidSSN("000-00-7890"));
-	assertFalse(SSN.isValidSSN("000-45-7890"));
-	assertFalse(SSN.isValidSSN("123-00-7890"));
-	assertFalse(SSN.isValidSSN("123-45-0000"));
 
-	assertTrue(SSN.isValidSSN("120-03-7890"));
-	assertTrue(SSN.isValidSSN("123-30-0890"));
-	assertTrue(SSN.isValidSSN("123-34-0006"));
-	assertTrue(SSN.isValidSSN("123-34-6000"));
+		assertFalse(SSN.isValidSSN("000-00-7890"));
+		assertFalse(SSN.isValidSSN("000-45-7890"));
+		assertFalse(SSN.isValidSSN("123-00-7890"));
+		assertFalse(SSN.isValidSSN("123-45-0000"));
+
+		assertTrue(SSN.isValidSSN("120-03-7890"));
+		assertTrue(SSN.isValidSSN("123-30-0890"));
+		assertTrue(SSN.isValidSSN("123-34-0006"));
+		assertTrue(SSN.isValidSSN("123-34-6000"));
 
 	}
-		
-	
+
 	@Test
-	public void validateSSNWithoutDash(){
-		
+	public void validateSSNWithoutDash() {
+
 		/** Middle **/
 		assertFalse(SSN.isValidSSN("111000789"));
 		assertFalse(SSN.isValidSSN("111000089"));
 		assertFalse(SSN.isValidSSN("110000089"));
 		assertTrue(SSN.isValidSSN("111100089"));
-		
+
 		/** First **/
 		assertTrue(SSN.isValidSSN("123456789"));
 		assertFalse(SSN.isValidSSN("000456789"));
@@ -59,7 +58,15 @@ public class SSNValidatorTest {
 		assertFalse(SSN.isValidSSN("123400000"));
 		assertTrue(SSN.isValidSSN("123400001"));
 		assertFalse(SSN.isValidSSN("000000000"));
+		assertFalse(SSN.isValidSSN("123004576"));
+		assertFalse(SSN.isValidSSN("023004576"));
+
+		assertFalse(SSN.isValidSSN("000345678"));
+		assertFalse(SSN.isValidSSN("06-648-1234"));
+		assertFalse(SSN.isValidSSN("666786543"));
 		
+		assertFalse(SSN.isValidSSN("800445678"));
+		assertFalse(SSN.isValidSSN("987654321"));
 	}
 
 }
