e985b748e0313f887a6a0e0d6a4e4bf4d3de6e1a
Digital Alchemist <developer.anwar@gmail.com>
2017-03-20 22:24:51 -0400

    RE code updated
    1- Test cases updated

---
+++ RegularExpression/src/main/java/com/dc/engineering/re/Problems.java
@@ -0,0 +1,13 @@
+package com.dc.engineering.re;
+
+public class Problems {
+/**
+ * http://www.mkyong.com/regular-expressions/domain-name-regular-expression-example/
+ * SSN
+ * EMAIL
+ * PHONE
+ * IP-ADDRESS
+ * HTML TAGS
+ * PASSWORD
+ */
+}
